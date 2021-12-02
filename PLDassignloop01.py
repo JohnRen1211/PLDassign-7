e1c2f888c7dc8d91ad1383d10b3314553769f159
JohnRen1211 <johnrengelojaylo@iskolarngbayan.pup.edu.ph>
2021-12-02 17:46:09 +0800

    Program for counting word

--- PLDassignloop01.py
+++ PLDassignloop01.py
@@ -23,3 +23,16 @@ for i in range(len(sentence)):
     if(sentence[i] == ' ' or sentence == '\n' or sentence == '\t'):
        word = word + 1  
 print("the total numbers of words=", word) 
+
+# Coding program for vowels and consonant
+
+for i in sentence:
+   if(ord(i) == 65 or ord(i) == 69 or ord(i) == 73
+        or ord(i) == 79 or ord(i) == 85
+        or ord(i) == 97 or ord(i) == 101 or ord(i) == 105
+        or ord(i) == 111 or ord(i) == 117):
+         vowels = vowels + 1
+   elif((ord(i) >= 97 and ord(i) <= 122) or (ord(i) >= 65 and ord(i) <= 90)):
+          consonants = consonants + 1
+
+# Printing the output of the program
